82ac5c047687d44dfb0a9d8683583e13c888df8a
Josh Duda <56557345+jduda27@users.noreply.github.com>
2020-02-07 11:17:32 -0600

    Update Memory.java

--- Project_1 -CPU_SIM/Memory.java
+++ Project_1 -CPU_SIM/Memory.java
@@ -1,12 +1,12 @@
 /** Operating Systems Project 1
 *   A memory class with 3 designated memory locations that can be accessed 
-*   securly by other classes.
+*   securely by other classes.
 *   @Author Joshua Duda
 *   @Date 2/5/2020
 */
 public class Memory {
 
-	//initiating the private varables that will be stored
+	//initiating the private variables that will be stored
 	private int mem3AE;
 	private int mem3AD;
 	private int mem3AC;
@@ -26,23 +26,25 @@ public class Memory {
 	*/
 	public int memGet(String memLoc) {
 		if (Integer.parseInt(memLoc) == 940) {
-			return mem3AC;
+			return getMem3AC();
 		} else if (Integer.parseInt(memLoc) == 941) {
-			return mem3AD;
+			return getMem3AD();
 		} else {
-			return mem3AE;
+			return getMem3AE();
 		}
 	}
 	
 	/**
 	 * set() method This method sets a specified value to the specified mem location
+	 * 
 	 * @precondition: String memLoc - a valid string describing the memory location
 	 *                hex int - a valid integer value represented in HEX
-	 * @postcondition: The method sets the hex to the correct memory location or if invalid it
-	 * 		   lets the user know it is not a valid memory location.
+	 * @postcondition: The method sets the hex to the correct memory location or if
+	 *                 invalid it lets the user know it is not a valid memory
+	 *                 location.
 	 */
 	public void memSet(String memLoc, int val) {
-		// checking for our three dsignated memory locations in decimal and using the setter 
+		// checking for our three designated memory locations in decimal and using the setter 
 		// methods to set the values to them.
 		if (Integer.parseInt(memLoc) == 940) {
 			setMem3AC(val);
@@ -53,6 +55,33 @@ public class Memory {
 		} else {
 			System.out.println("Not a valid memory location: " + memLoc);
 		}
+	}
+	
+	/**
+	 * getMem3AC() method This method gets and returns the memory stored in location 3AC
+	 * @precondition: none;
+	 * @postcondition: Returns the hex value stored at memory 3AC (940 decimal)
+	 */
+	public int getMem3AC() {
+		return mem3AC;
+	}
+	
+	/**
+	 * getMem3AD() method This method gets and returns the memory stored in location 3AD
+	 * @precondition: none;
+	 * @postcondition: Returns the hex value stored at memory 3AD (941 decimal)
+	 */
+	public int getMem3AD() {
+		return mem3AD;
+	}
+	
+	/**
+	 * getMem3AE() method This method gets and returns the memory stored in location 3AE
+	 * @precondition: none;
+	 * @postcondition: Returns the hex value stored at memory 3AE (942 decimal)
+	 */
+	public int getMem3AE() {
+		return mem3AE;
 	}
 	
 	/** setMem3AC() method - this method is a setter method that takes in a valid hex integer
